9a774b53c5faa8ee06649c7bbb03f4615ff0caca
luke.sims <lukesims12@gmail.com>
2020-06-09 15:59:08 +0100

    Added github to header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,9 @@ export default class Header extends Component {
                 <div className="title-section">
                     <h1 className="main-title">try-catch-finally</h1>
                     <h2 className="sub-title">a collection of javascript projects</h2>
+                    <div className="social-icons-div">
+                        <div className="social-icon"><a target="_blank" rel="noopener noreferrer" href="https://github.com/lukesims12"><i className="fab fa-github githubIcon"></i></a></div>
+                    </div>
                 </div>
 
                 {/* <div className="wave-container">
